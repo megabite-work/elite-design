7c88804933412b4e0e8f41115241dd9f82e8a305
megabite-work <g.work.01.01.2000@gmail.com>
2024-10-16 18:22:43 +0500

    add postman collection

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -54,8 +54,8 @@ class ProjectController extends BaseController implements HasMiddleware
         }
 
         $data = $request->only(['title', 'short_description', 'city', 'year', 'description', 'files', 'image', 'pictures', 'characteristics', 'plans', 'plan_photos', 'video', 'address', 'longitude', 'latitude']);
-        
-        foreach ($request->files as $key => $file) {
+
+        foreach ($data['files'] as $key => $file) {
             $data['files'][$key] = MediaObject::upload($file, 'files');
         }
         ksort($data['files']);
@@ -99,24 +99,24 @@ class ProjectController extends BaseController implements HasMiddleware
         }
 
         $validator = Validator::make($request->all(), [
-            'title' => ['bail', 'required', 'string', 'max:255'],
-            'short_description' => ['bail', 'required', 'string'],
-            'city' => ['bail', 'required', 'string', 'max:255'],
-            'year' => ['bail', 'required', 'integer'],
-            'description' => ['bail', 'required', 'string'],
-            'files' => ['bail', 'required', 'array'],
-            'files.*' => ['bail', 'required', 'file', 'max:10240'],
-            'image' => ['bail', 'required', 'image', 'max:10240'],
-            'pictures' => ['bail', 'required', 'array'],
-            'pictures.*' => ['bail', 'required', 'image', 'max:10240'],
-            'characteristics' => ['bail', 'required', 'string'],
-            'plans' => ['bail', 'required', 'string'],
-            'plan_photos' => ['bail', 'required', 'array'],
-            'plan_photos.*' => ['bail', 'required', 'image', 'max:10240'],
-            'video' => ['bail', 'required', 'string'],
-            'address' => ['bail', 'required', 'string'],
-            'longitude' => ['bail', 'required', 'string'],
-            'latitude' => ['bail', 'required', 'string'],
+            'title' => ['bail', 'nullable', 'string', 'max:255'],
+            'short_description' => ['bail', 'nullable', 'string'],
+            'city' => ['bail', 'nullable', 'string', 'max:255'],
+            'year' => ['bail', 'nullable', 'integer'],
+            'description' => ['bail', 'nullable', 'string'],
+            'files' => ['bail', 'nullable', 'array'],
+            'files.*' => ['bail', 'nullable', 'file', 'max:10240'],
+            'image' => ['bail', 'nullable', 'image', 'max:10240'],
+            'pictures' => ['bail', 'nullable', 'array'],
+            'pictures.*' => ['bail', 'nullable', 'image', 'max:10240'],
+            'characteristics' => ['bail', 'nullable', 'string'],
+            'plans' => ['bail', 'nullable', 'string'],
+            'plan_photos' => ['bail', 'nullable', 'array'],
+            'plan_photos.*' => ['bail', 'nullable', 'image', 'max:10240'],
+            'video' => ['bail', 'nullable', 'string'],
+            'address' => ['bail', 'nullable', 'string'],
+            'longitude' => ['bail', 'nullable', 'string'],
+            'latitude' => ['bail', 'nullable', 'string'],
         ]);
 
         if ($validator->fails()) {
